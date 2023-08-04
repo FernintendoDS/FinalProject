fe193a52d0db12a2ca11d3078e0aa5e27ec86a40
FernintendoDS <105448150+FernintendoDS@users.noreply.github.com>
2023-08-03 22:12:53 -0400

    Update AM.py

--- AM.py
+++ AM.py
@@ -4,7 +4,19 @@ import struct
 arduino = serial.Serial('/dev/ttyACM1', 9600, timeout=1.0)
 time.sleep(3)
 
+def arduino_send(data):
+    arduino.write(data.encode())
+
+def arduino_read():
+    data = arduino.readline().decode().strip()
+    return data
+
 def get_ultrasonic_front():
+    arduino.write('u')
+    data = arduino_read()
+    return data
 
 def get_motor_dir():
-
+    arduino.write('m')
+    data = arduino_read()
+    return data
