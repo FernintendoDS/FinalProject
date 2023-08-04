76935454e8b60214827737c92912d02cf335b5b2
FernintendoDS <105448150+FernintendoDS@users.noreply.github.com>
2023-08-03 22:18:35 -0400

    Update arduino_leonardo.ino

--- arduino_leonardo.ino
+++ arduino_leonardo.ino
@@ -1,1 +1,23 @@
+#include <Wire.h>
 
+void setup() {
+  Serial.begin(9600);
+
+  
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char data = Serial.read();
+
+    if (data == 'u') {
+      read_all_ultrasonic();
+    } 
+    
+  }
+}
+
+read_all_ultrasonic() {
+
+    
+}
