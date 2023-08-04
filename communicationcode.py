e2382586c1fe8b9da54435e3cb49f741454ebb35
Pukhraj Singh <105447728+pukhrajcheema@users.noreply.github.com>
2023-08-03 20:48:46 -0400

    Update communicationcode.py

--- communicationcode.py
+++ communicationcode.py
@@ -1,1 +1,18 @@
+import serial
+import time
+import struct
+arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1.0)
+time.sleep(3)
 
+def send_command():
+    while True:
+        line = arduino.readline().decode().strip()
+        
+        print(line)
+    
+
+send_command()
+    
+arduino.close()
+    
+    
