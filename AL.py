709a8bddcce6974d61877adc9c4491e8a6331830
FernintendoDS <105448150+FernintendoDS@users.noreply.github.com>
2023-08-03 20:57:38 -0400

    Update AL.py

--- AL.py
+++ AL.py
@@ -1,10 +1,16 @@
+import serial
+import time
+import struct
+arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1.0)
+time.sleep(3)
 
-def get_imu_back():
+def get_imu():
 
-def get_ultrasonic():
+def get_ultrasonic_back():
 
 def motor_control():
 
 def fan_control():
 
 def get_temp():
+
