bd3a3a61e77388d847c3296192e483bab84b9617
FernintendoDS <105448150+FernintendoDS@users.noreply.github.com>
2023-08-03 20:57:53 -0400

    Update AM.py

--- AM.py
+++ AM.py
@@ -1,5 +1,10 @@
-def get_imu_front():
+import serial
+import time
+import struct
+arduino = serial.Serial('/dev/ttyACM1', 9600, timeout=1.0)
+time.sleep(3)
+
+def get_ultrasonic_front():
 
 def get_motor_dir():
-
 
