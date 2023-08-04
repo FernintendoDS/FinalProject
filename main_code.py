9dafbca668af5c87e03e6b75783aa85c1f84200c
Pukhraj Singh <105447728+pukhrajcheema@users.noreply.github.com>
2023-08-03 20:32:38 -0400

    Update main_code.py
    added the parent class that will include all the functions

--- main_code.py
+++ main_code.py
@@ -1,1 +1,13 @@
 import adruino_mega.py
+import addfunction
+
+
+def mainfunction():
+    d = addfunction.sum(4,5)
+    e = addfunction.subtract(5,4)
+
+    print(d)
+    print(e)
+   
+
+mainfunction()
