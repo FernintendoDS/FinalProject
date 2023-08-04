af39e163617d7462577aee85cf44be2e2bc720fa
FernintendoDS <105448150+FernintendoDS@users.noreply.github.com>
2023-08-03 21:57:30 -0400

    Update AL.py

--- AL.py
+++ AL.py
@@ -4,13 +4,35 @@ import struct
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1.0)
 time.sleep(3)
 
-def get_imu():
+def arduino_send(data):
+    arduino.write(data.encode())
 
+def arduino_read():
+    data = arduino.readline().decode().strip()
+    return data
+
+def get_imu():
+    arduino.write('i')
+    data = arduino_read()
+    return data
+    
 def get_ultrasonic_back():
+    arduino.write('u')
+    data = arduino_read()
+    return data
 
 def motor_control():
+    arduino.write('m')
+    data = arduino_read()
+    return data
 
 def fan_control():
+    arduino.write('f')
+    data = arduino_read()
+    return data
 
 def get_temp():
+    arduino.write('t')
+    data = arduino_read()
+    return data
 
