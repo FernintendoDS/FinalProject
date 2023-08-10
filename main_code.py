648e44b3ff469520fa7aa8b900a742207252a734
FernintendoDS <105448150+FernintendoDS@users.noreply.github.com>
2023-08-10 12:26:24 -0400

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -2,21 +2,27 @@ import AM
 import AL
 import re
 
-def extract_data(d):
-    pattern = r"U2 (\d+)"
+def extract_data(d, tag):
+    pattern = fr"{tag} (\d+)"
     match = re.search(pattern, d)
     if match:
-        u1_match = match.group(1)
+        match = match.group(1)
 
-        return u1_match
-        
+        return match
     else:
         return 
+
     
 def mainnew():
     while True:
-        d = AL.get_ultrasonic_back()
-        dataU1 = extract_data(d)
-        print(dataU1)
-
+        d = AL.get_ultrasonic_back1()
+        dataU1 = extract_data(d, "U1")
+        print("U1 "+dataU1)
+        b = AL.get_ultrasonic_back2()
+        dataU2 = extract_data(b, "U2")
+        print("U2 "+dataU2)
+        c = AL.get_ultrasonic_back3()
+        dataU3 = extract_data(c, "U3")
+        print("U3 "+dataU3)
+       
 mainnew()
